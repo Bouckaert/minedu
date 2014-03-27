0df76b1bb64df8ef6193063ffd33204e7f29183e
Bouckaert <yhsmsb@cin.ufpe.br>
2014-03-27 18:50:10 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minedu::Application.config.secret_key_base = '10ae1510c271c31a31c13a5ec0c70ffcbece4c304d2b74b392b75d452fd020083ff9d7cc7288bcd56fe1c8db93ce9b3055008db6b04ddfea2ce133046c99de33'
